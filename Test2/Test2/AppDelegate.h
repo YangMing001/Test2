8ebdd4f774bbeb2942059a9299b13dff81b6d00e
YM <yangming@1yd.me>
2016-06-08 11:42:04 +0800

    init

---
+++ Test2/Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by YM on 16/6/8.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
